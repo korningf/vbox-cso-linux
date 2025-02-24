ade4e27ad9a245fdf129e9805de5a36937bcdbcf
FK <francis.korning@cso.ie>
2025-02-24 15:54:04 +0000

    aws ec2 instance id and name

--- boot.sh
+++ boot.sh
@@ -4,10 +4,14 @@
 mkdir -p /etc/apt/keyrings
 
 # apt repositories 
+apt update
 
 
 # apt packages
-apt update
+
+# aws-cli
+snap install aws-cli --classic
+
 
 # banner (motd) 
 curl  https://raw.githubusercontent.com/korningf/vbox-cso-linux/refs/heads/main/motd > /etc/motd
@@ -16,7 +20,10 @@ curl  https://raw.githubusercontent.com/korningf/vbox-cso-linux/refs/heads/main/
 REGION=us-east-1
 
 # variables
-AWS_INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
-EC2_NAME=$(aws ec2 describe-tags --region $REGION --filters "Name=resource-id,Values=$AWS_INSTANCE_ID" "Name=key,Values=Name" --output text | cut -f5)
+AWS_EC2_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
+AWS_EC2_NAME=$(aws ec2 describe-tags --region $REGION --filters "Name=resource-id,Values=$AWS_EC2_ID" "Name=key,Values=Name" --output text | cut -f5)
+
+echo $AWS_EC2_ID >> /etc/aws_ec2_id
+echo $AWS_EC2_NAME >> /etc/aws_ec2_name
 
 
